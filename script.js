50658fa372c9fa7759f6c05ce3b266282d275690
Robert Mudron <robino1498@gmail.com>
2026-04-09 13:19:42 +0200

    Refactor the code into functions

--- script.js
+++ script.js
@@ -41,78 +41,110 @@ function operate(firstNumber, secondNumber, operator) {
 
 }
 
-function clearDisplay() {
+calculator = {
+    displayValue: 0,
+    firstNumber: "",
+    secondNumber: "",
+    operator: "",
+    operatorSelected: false,
+    isEqualsDisplayed: false,
+    isDecimalUsed: false,
+}
 
-    display.textContent = 0;
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    operatorSelected = false;
-    isEqualsDisplayed = false;
-    isDecimalUsed = false;
+function resetState() {
+
+    calculator.displayValue = 0;
+    calculator.firstNumber = "";
+    calculator.secondNumber = "";
+    calculator.operator = "";
+    calculator.operatorSelected = false;
+    calculator.isEqualsDisplayed = false;
+    calculator.isDecimalUsed = false;
 
 }
 
-let buttons = document.querySelector(".clickable");
-let display = document.querySelector(".display");
+const buttons = document.querySelector(".clickable");
+const display = document.querySelector(".display");
 
-clearDisplay();
+function updateDisplay() {
+    display.textContent = calculator.displayValue;
+}
+
+updateDisplay();
 
 buttons.addEventListener("click", (e) => {
+    const target = e.target;
+    const { displayValue } = calculator;
 
-    if (display.textContent === "DIV0") clearDisplay();
-
-    if (e.target.className == 'num') {
-        if (isEqualsDisplayed) {
-            clearDisplay();
-        }
-        if (!(display.textContent == 0 && e.target.textContent == 0) && (!isDecimalUsed || e.target.id != 'decimal')) {
-            if (e.target.id === 'decimal') {
-                isDecimalUsed = true;
-            }
-            if (operatorSelected) {
-                if (secondNumber.length < 10) {
-                    secondNumber += e.target.textContent;
-                    display.textContent = secondNumber;
-                }
-            } else {
-                if (firstNumber.length < 10) {
-                    firstNumber += e.target.textContent;
-                    display.textContent = firstNumber;
-                }
-            }
-        }
-    } else if (e.target.className = 'operator') {
-        if (isEqualsDisplayed) {
-            isEqualsDisplayed = false;
-        }
-
-        if (e.target.id == 'ac') {
-            clearDisplay();
-        } else {
-
-            if (secondNumber) {
-                firstNumber = operate(firstNumber, secondNumber, operator);
-                if (firstNumber === Infinity) {
-                    display.textContent = "DIV0";
-                } else {
-                    display.textContent = firstNumber;
-                    secondNumber = "";
-                }
-            }
-            // Save operator post evaluation so that it doesn't leak into the current result
-            if (e.target.id != 'equals') {
-                operator = e.target.textContent;
-                operatorSelected = true;
-            } else {
-                isEqualsDisplayed = true;
-            }
-            isDecimalUsed = false;
-        }
+    if (displayValue === "DIV0" || target.id == 'ac') {
+        resetState();
+        updateDisplay()
+        return;
     }
+
+    if (target.className === 'num') {
+        handleInput(target);
+        updateDisplay()
+        return;
+
+    }
+
+    // handles operator
+    handleOperator(target);
+    updateDisplay();
 })
 
 
+function handleInput(target) {
+    const { isEqualsDisplayed, isDecimalUsed, firstNumber, secondNumber, operatorSelected, displayValue } = calculator;
+
+    if (isEqualsDisplayed) {
+        clearDisplay();
+    }
+    if (!(displayValue == 0 && target.textContent == 0) && (!isDecimalUsed || target.id != 'decimal')) {
+        if (target.id === 'decimal') {
+            calculator.isDecimalUsed = true;
+        }
+        if (operatorSelected) {
+            if (secondNumber.length < 10) {
+                calculator.secondNumber += target.textContent;
+                calculator.displayValue = calculator.secondNumber;
+            }
+        } else {
+            if (firstNumber.length < 10) {
+                calculator.firstNumber += target.textContent;
+                calculator.displayValue = calculator.firstNumber;
+            }
+        }
+    }
+}
 
 
+function handleOperator(target) {
+    const { isEqualsDisplayed, firstNumber, secondNumber, operator } = calculator;
 
+    if (isEqualsDisplayed) {
+        calculator.isEqualsDisplayed = false;
+    }
+
+    else {
+
+        if (secondNumber) {
+            calculator.firstNumber = operate(firstNumber, secondNumber, operator);
+            if (calculator.firstNumber == Infinity) {
+                calculator.displayValue = "DIV0";
+            } else {
+                calculator.displayValue = calculator.firstNumber;
+                calculator.secondNumber = "";
+            }
+        }
+        // Save operator post evaluation so that it doesn't leak into the current result
+        if (target.id != 'equals') {
+            calculator.operator = target.textContent;
+            calculator.operatorSelected = true;
+        } else {
+            calculator.isEqualsDisplayed = true;
+        }
+        calculator.isDecimalUsed = false;
+    }
+}
